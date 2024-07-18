f4e9928f963f729c52b7fc5b8035daa54043fcbf
rangeinvesting <123790483+rangeinvesting@users.noreply.github.com>
2024-07-18 19:36:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+    scrapeLogic(res);
 });
 
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+    res.send("Render Puppeteer server is up and running!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
